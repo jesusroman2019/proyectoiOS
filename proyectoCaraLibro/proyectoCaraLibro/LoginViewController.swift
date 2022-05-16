e608f2bec6f8411e972d1bfcb6290c5f4457990d
Jesus Roman <jesusromanl2488@gmail.com>
2022-05-15 23:15:10 -0400

    Avance 3 - Login - avanzando teclado

---
+++ proyectoCaraLibro/proyectoCaraLibro/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  proyectoCaraLibro
+//
+//  Created by user191022 on 5/15/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var txtUser: UITextField!
+    
+    @IBAction private func tapToCloseKeyboard(sender: UITapGestureRecognizer) {
+            self.view.endEditing(true)
+        }
+    
+    @IBAction private func swipeToCloseKeyboard(_ sender: UISwipeGestureRecognizer) {
+            self.view.endEditing(true)
+        }
+    
+    @IBAction private func swipeToOpenKeyboard(_ sender: UISwipeGestureRecognizer) {
+            self.txtUser.becomeFirstResponder()
+        }
+    
+    override func viewWillAppear(_ animated: Bool) {
+            //super.viewWillAppear(animated)
+            //self.registerKeyboardNotifications()
+        }
+
+        override func viewWillDisappear(_ animated: Bool) {
+            //super.viewWillDisappear(animated)
+            //self.unregisterKeyboardNotifications()
+        }
+    
+}
---
+++ proyectoCaraLibro/proyectoCaraLibro/NewUserViewController.swift
@@ -0,0 +1,8 @@
+//
+//  NewUserViewController.swift
+//  proyectoCaraLibro
+//
+//  Created by user191022 on 5/15/22.
+//
+
+import Foundation
