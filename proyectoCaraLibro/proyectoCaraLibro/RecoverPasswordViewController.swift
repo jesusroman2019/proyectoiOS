fe0c8914e1e41b9fa785d466cba891804aeb8bc7
Joseph Mejia <mejia_123_j@hotmail.com>
2022-05-20 04:05:57 -0400

    contrase;a

--- proyectoCaraLibro/proyectoCaraLibro/RecoverPasswordViewController.swift
+++ proyectoCaraLibro/proyectoCaraLibro/RecoverPasswordViewController.swift
@@ -8,62 +8,38 @@
 import UIKit
 
 class RecoverPasswordViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet private weak var AnchorViewContent: NSLayoutConstraint!
+    @IBOutlet private weak var viewContent: UIView!
     
-    
-    @IBOutlet weak var scrollView: UIScrollView!
-    
-    @IBOutlet weak var txtEmail: UITextField!
-    
-    @IBAction private func tapToCloseKeyboard(sender: UITapGestureRecognizer) {
-            self.view.endEditing(true)
-        }
-    
-    @IBAction private func swipeToCloseKeyboard(_ sender: UISwipeGestureRecognizer) {
-            self.view.endEditing(true)
-        }
-    
-    @IBAction private func swipeToOpenKeyboard(_ sender: UISwipeGestureRecognizer) {
-            self.txtEmail.becomeFirstResponder()
-        }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //self.HideKeyboard()
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(Keyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(Keyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-        
+    @IBAction private func tapToCloseKeyboard(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
     }
-        
     
     override func viewWillAppear(_ animated: Bool) {
-            //super.viewWillAppear(animated)
-            //self.registerKeyboardNotifications()
-        }
-
-        override func viewWillDisappear(_ animated: Bool) {
-            //super.viewWillDisappear(animated)
-            //self.unregisterKeyboardNotifications()
-        }
-    
-    @objc func Keyboard(notification: Notification){
-        
-        //let userInfo = notification.userInfo!
-        
-        //let keyboardScreenEndFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey]) as! NSValue).cgRectValue
-        let keyboardScreenEndFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
-        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
-        
-        if notification.name == UIResponder.keyboardWillHideNotification {
-            scrollView.contentInset = UIEdgeInsets.zero
-        } else {
-            scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
-        }
-        
-        scrollView.scrollIndicatorInsets = scrollView.contentInset
-        
+        super.viewWillAppear(animated)
+        self.registerKeyBoardEvents()
     }
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unregisterKeyBoardEvents()
+    }
+    
+    private func registerKeyBoardEvents() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    private func unregisterKeyBoardEvents() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    @objc private func keyboardWillShow(_ notification: Notification){
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+        if keyboardFrame.origin.y < self.viewContent.frame.maxY {
+            self.AnchorViewContent.constant = keyboardFrame.origin.y - self.viewContent.frame.maxY
+        }
+    }
+    @objc private func keyboardWillHide(_ notification: Notification){
+        self.AnchorViewContent.constant = 0
+    }
 }
