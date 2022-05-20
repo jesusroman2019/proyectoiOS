aaab10344c40fe03293d3cef3957a01e0c7175af
Jesus Roman <jesusromanl2488@gmail.com>
2022-05-19 22:26:19 -0400

    avanzando

--- proyectoCaraLibro/proyectoCaraLibro/LoginViewController.swift
+++ proyectoCaraLibro/proyectoCaraLibro/LoginViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController{
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    //scrollView.contentSize =
     
     
     @IBOutlet weak var txtUser: UITextField!
@@ -24,6 +29,14 @@ class LoginViewController: UIViewController {
             self.txtUser.becomeFirstResponder()
         }
     
+    override func viewDidLoad() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    
+    
     override func viewWillAppear(_ animated: Bool) {
             //super.viewWillAppear(animated)
             //self.registerKeyboardNotifications()
@@ -34,4 +47,19 @@ class LoginViewController: UIViewController {
             //self.unregisterKeyboardNotifications()
         }
     
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            //yourTextView.contentInset = .zero
+        } else {
+            //yourTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        //yourTextView.scrollIndicatorInsets = yourTextView.contentInset
+
+            }
 }
