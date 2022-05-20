9e01bd8857e56c1d48601f2bc4e40b410ed5a1d9
Jesus Roman <jesusromanl2488@gmail.com>
2022-05-20 00:55:30 -0400

    + gestos

--- proyectoCaraLibro/proyectoCaraLibro/NewUserViewController.swift
+++ proyectoCaraLibro/proyectoCaraLibro/NewUserViewController.swift
@@ -9,4 +9,17 @@ import UIKit
 
 class NewUserViewController: UIViewController, UITextFieldDelegate{
     
-}
+    
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBAction private func tapToCloseKeyboard(sender: UITapGestureRecognizer) {
+            self.view.endEditing(true)
+        }
+    
+    @IBAction private func swipeToCloseKeyboard(_ sender: UISwipeGestureRecognizer) {
+            self.view.endEditing(true)
+        }
+    
+    @IBAction private func swipeToOpenKeyboard(_ sender: UISwipeGestureRecognizer) {
+            self.txtName.becomeFirstResponder()
+        }}
