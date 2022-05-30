ba9d1bd6347054100274cb30e322f8abc010a13f
Jesus Roman <jesusromanl2488@gmail.com>
2022-05-29 20:26:39 -0400

    Login User

---
+++ proyectoCaraLibro/proyectoCaraLibro/LoginUserViewController.swift
@@ -0,0 +1,70 @@
+//
+//  LoginUserViewController.swift
+//  proyectoCaraLibro
+//
+//  Created by user191022 on 5/29/22.
+//
+
+import UIKit
+
+class LoginUserViewController: UIViewController {
+    
+    @IBOutlet private weak var anchorBottomScroll: NSLayoutConstraint!
+    
+    @IBAction private func tapToCloseKeyboard(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.registerKeyboardNotification()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unregisterKeyboardNotification()
+    }
+    
+    
+}
+
+
+//MARK: - Keyboard events
+extension LoginUserViewController {
+    
+    private func registerKeyboardNotification() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    private func unregisterKeyboardNotification() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.anchorBottomScroll.constant = keyboardFrame.height
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.anchorBottomScroll.constant = 0
+            self.view.layoutIfNeeded()
+        }
+    }
+}
