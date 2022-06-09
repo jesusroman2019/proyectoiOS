5225f16997b00f3d020bf7555ec063c336dd2452
Rodrigo Prieto <rodrigoprietoguevara@gmail.com>
2022-06-08 23:31:54 -0400

    Login Fire

--- proyectoCaraLibro/proyectoCaraLibro/LoginViewController.swift
+++ proyectoCaraLibro/proyectoCaraLibro/LoginViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import FirebaseAnalytics
+import FirebaseAuth
 
 class LoginViewController: UIViewController, UITextFieldDelegate{
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
     
     @IBOutlet weak var scrollView: UIScrollView!
     
@@ -68,5 +73,21 @@ class LoginViewController: UIViewController, UITextFieldDelegate{
         scrollView.scrollIndicatorInsets = scrollView.contentInset
     }
     
+    @IBAction func signUpButtonAction(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            Auth.auth().signIn(withEmail: email, password: password){
+                (result, error) in
+                
+                if let result = result, error == nil{
+                    self.navigationController? .pushViewController(MenuViewController(), animated: true)
+                    
+                } else{
+                    let alertController = UIAlertController(title: "Error", message: "Se ha producido un error", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
     
 }
