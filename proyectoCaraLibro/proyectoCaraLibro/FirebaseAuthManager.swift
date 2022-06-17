d3a49e08238a5111961c3e10ac41911bbcd593c3
Jesus Roman <jesusromanl2488@gmail.com>
2022-06-16 23:48:42 -0400

    faltan funciones

---
+++ proyectoCaraLibro/proyectoCaraLibro/FirebaseAuthManager.swift
@@ -0,0 +1,14 @@
+//
+//  FirebaseAuthManager.swift
+//  proyectoCaraLibro
+//
+//  Created by user191554 on 6/16/22.
+//
+
+import UIKit
+import FirebaseAuth
+
+class FirebaseAuthManager{
+    
+        
+}
