1605b0ab6cfccca10c02a494243c2b0b63310744
Jesus Roman <jesusromanl2488@gmail.com>
2022-06-18 22:58:21 -0400

    registro avanzando

--- proyectoCaraLibro/proyectoCaraLibro/NewUserViewController.swift
+++ proyectoCaraLibro/proyectoCaraLibro/NewUserViewController.swift
@@ -6,13 +6,68 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class NewUserViewController: UIViewController {
     
     @IBOutlet private weak var anchorBottomScroll: NSLayoutConstraint!
     
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var btnAddPhotoButton: UIButton!
+    @IBOutlet weak var btnSaveButton: UIButton!
+    
     @IBAction private func tapToCloseKeyboard(sender: UITapGestureRecognizer) {
         self.view.endEditing(true)
+    }
+    
+    @IBAction func signUpButtonAction(_ sender: Any){
+        
+        if let email = emailTextField.text, let password = passwordTextField.text
+        
+        {
+            Auth.auth().createUser(withEmail: email, password: password) {
+                (result, error) in
+                
+                if let result = result, error == nil {
+                    
+                    let storyBoard : UIStoryboard = UIStoryboard(name: "Second", bundle:nil)
+                    let resultViewController = storyBoard.instantiateViewController(withIdentifier: "contactos") as! HomeViewController
+                    self.navigationController?
+                        .pushViewController(resultViewController, animated: true)
+                        print(result)
+                } else {
+                    
+                    let alertController = UIAlertController(title: "Error",
+                        message:
+                        "Se ha producido un error",
+                            preferredStyle: .alert)
+                        alertController.addAction(UIAlertAction(title: "Aceptar",
+                            style: .default))
+                            
+                        self.present(alertController, animated: true, completion: nil)
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let ref = Database.database().reference()
+        
+        ref.child("users").setValue(["email":"test1@gmail.com","password":"654321"])
+        
+        //ref.childByAutoId().setValue(["email":"test1@gmail.com","password":"654321"])
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
